f9d2344c70c3e64ab7844599f7cb94acac55f9a3
ipota <kiratoki512810@gmail.com>
2025-10-05 16:01:48 +0900

    mode

--- src/Game/Scene.ts
+++ src/Game/Scene.ts
@@ -12,6 +12,7 @@ import mapdata from "../../assets/mapdata/mapdata.json"
 
 export class Scene {
     readonly ready
+
     readonly camera
     readonly map
 
@@ -19,13 +20,8 @@ export class Scene {
     readonly items: string[] = []
 
     readonly #ctx
-    #mode: "play" | "event" | "menu" = "event"
 
-    readonly #menu = {
-        index: 0,
-    }
-
-    #modeEvent!: ModeEvent
+    #mode: Mode
 
     constructor() {
         const cvs = document.createElement("canvas")
@@ -42,89 +38,20 @@ export class Scene {
         this.map = new MapData(mapdata, new Player(this), this)
         // this.map = new MapData(mapdata, new MapWriter(this), this)
 
-        this.#modeEvent = new ModeEvent([new EventFirst(this)])
+        this.#mode = new ModeEvent(this, [new EventFirst(this)])
 
         this.ready = this.map.ready
     }
 
     update() {
-        if (this.#mode === "play") {
-            this.#modePlay()
-            this.#draw()
-        } else if (this.#mode === "event") {
-            this.#draw()
+        const next = this.#mode.update()
 
-            if (this.#modeEvent.update()) {
-                this.#mode = "play"
-            }
-        } else if (this.#mode === "menu") {
-            this.#modeMenu()
+        if (next) {
+            this.#mode = next
         }
 
+        this.#draw()
         Input.update()
-    }
-
-    #modePlay() {
-        this.map.realSprites.forEach((s) => s.update(this))
-
-        if (keyboard.pushed.has("KeyZ")) {
-            this.#action()
-        }
-
-        if (keyboard.pushed.has("KeyX")) {
-            this.#gotoMenu()
-        }
-    }
-
-    #action() {
-        const event = this.map.realSprites
-            .filter((s) => s.getExistsP().some((p) => p.equals(this.map.player.getDirectedP())))
-            .map((s) => s.action(this.map, this))
-            .filter((s) => s instanceof GameEvent)
-
-        if (event.length > 0) {
-            this.#modeEvent = new ModeEvent(event)
-            this.#mode = "event"
-        }
-    }
-
-    #gotoMenu() {
-        this.#mode = "menu"
-        this.#menu.index = 0
-        document.querySelectorAll(`#menu #left #buttons button`).forEach((b) => b.classList.remove("selected"))
-
-        document.querySelector("#menu")?.classList.remove("hidden")
-        document.querySelector(`#menu #left #buttons :nth-child(${1})`)?.classList.add("selected")
-    }
-
-    #modeMenu() {
-        if (keyboard.pushed.has("KeyX")) {
-            this.#mode = "play"
-            document.querySelector("#menu")?.classList.add("hidden")
-        }
-
-        if (keyboard.longPressed.has("ArrowUp")) {
-            this.#menu.index = (this.#menu.index + 2) % 3
-            document.querySelectorAll(`#menu #left #buttons button`).forEach((b) => b.classList.remove("selected"))
-            document
-                .querySelector(`#menu #left #buttons :nth-child(${this.#menu.index + 1})`)
-                ?.classList.add("selected")
-        }
-
-        if (keyboard.longPressed.has("ArrowDown")) {
-            this.#menu.index = (this.#menu.index + 1) % 3
-            document.querySelectorAll(`#menu #left #buttons button`).forEach((b) => b.classList.remove("selected"))
-            document
-                .querySelector(`#menu #left #buttons :nth-child(${this.#menu.index + 1})`)
-                ?.classList.add("selected")
-        }
-
-        if (keyboard.pushed.has("KeyZ")) {
-            if (this.#menu.index === 0) {
-                this.#mode = "play"
-                document.querySelector("#menu")?.classList.add("hidden")
-            }
-        }
     }
 
     #draw() {
@@ -143,11 +70,48 @@ export class Scene {
     }
 }
 
-class ModeEvent {
+class Mode {
+    sceneRef
+
+    get scene() {
+        return this.sceneRef.deref()!
+    }
+
+    constructor(scene: Scene) {
+        this.sceneRef = new WeakRef(scene)
+    }
+
+    update(): Mode | void {}
+}
+
+class ModePlay extends Mode {
+    update(): Mode | void {
+        this.scene.map.realSprites.forEach((s) => s.update(this.scene))
+
+        if (keyboard.pushed.has("KeyZ")) {
+            const events = this.scene.map.realSprites
+                .filter((s) => s.getExistsP().some((p) => p.equals(this.scene.map.player.getDirectedP())))
+                .map((s) => s.action(this.scene.map, this))
+                .filter((s) => s instanceof GameEvent)
+
+            if (events.length > 0) {
+                return new ModeEvent(this.scene, events)
+            }
+        }
+
+        if (keyboard.pushed.has("KeyX")) {
+            return new ModeMenu(this.scene)
+        }
+    }
+}
+
+class ModeEvent extends Mode {
     #currentEvent: GameEvent
     #eventQueue: GameEvent[] = []
 
-    constructor(events: readonly GameEvent[]) {
+    constructor(scene: Scene, events: GameEvent[]) {
+        super(scene)
+
         const copy = [...events]
 
         this.#currentEvent = copy.shift()!
@@ -159,9 +123,87 @@ class ModeEvent {
 
         if (done) {
             if (this.#eventQueue.length === 0) {
-                return true
+                return new ModePlay(this.scene)
             } else {
                 this.#currentEvent = this.#eventQueue.shift()!
+            }
+        }
+    }
+}
+
+class ModeMenu extends Mode {
+    #container = document.createElement("div")
+    #index = 0
+
+    constructor(scene: Scene) {
+        super(scene)
+
+        this.#container.id = "menu"
+        this.#container.className = "hidden"
+
+        this.#container.innerHTML = `
+            <div id="left">
+                <div id="buttons">
+                    <button>再開</button>
+                    <button>持物</button>
+                    <button>終了</button>
+                </div>
+
+                <div id="money">0円</div>
+            </div>
+            <div id="right">
+                <div class="character">
+                    <img class="status-icon" src="assets/images/icon/ユウナ.png" alt="" />
+                    <div class="status">
+                        ユウナ <br />
+                        LV: 9 <br />
+                        HP: 9 MP: 0 SAN: 20<br />
+                    </div>
+                </div>
+            </div>
+        `
+
+        document.querySelector("#container")!.appendChild(this.#container)
+
+        this.#getButton(0).classList.add("selected")
+
+        requestAnimationFrame(() => {
+            this.#container.classList.remove("hidden")
+        })
+    }
+
+    #getButton(index: number) {
+        return this.#container.querySelector(`#left #buttons button:nth-child(${index + 1})`)!
+    }
+
+    #getAllButton() {
+        return this.#container.querySelectorAll(`#left #buttons button`)!
+    }
+
+    update() {
+        if (keyboard.pushed.has("KeyX")) {
+            this.#container.ontransitionend = () => this.#container.remove()
+            this.#container.classList.add("hidden")
+            return new ModePlay(this.scene)
+        }
+
+        if (keyboard.longPressed.has("ArrowUp")) {
+            this.#index = (this.#index + 2) % 3
+            this.#getAllButton().forEach((b) => b.classList.remove("selected"))
+            this.#getButton(this.#index).classList.add("selected")
+        }
+
+        if (keyboard.longPressed.has("ArrowDown")) {
+            this.#index = (this.#index + 1) % 3
+            this.#getAllButton().forEach((b) => b.classList.remove("selected"))
+            this.#getButton(this.#index).classList.add("selected")
+        }
+
+        if (keyboard.pushed.has("KeyZ")) {
+            if (this.#index === 0) {
+                this.#container.ontransitionend = () => this.#container.remove()
+                document.querySelector("#menu")?.classList.add("hidden")
+                return new ModePlay(this.scene)
             }
         }
     }
